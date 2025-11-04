179577538212df69a171237275460265ba7b044d
Connor Garey <connorgarey1999@gmail.com>
2025-11-04 17:20:01 +0000

    Initialised with size of client_address_ member

--- include/server/acceptor.h
+++ include/server/acceptor.h
@@ -18,7 +18,7 @@ private:
   void initAccept(const int& file_descriptor);
 
   sockaddr_storage client_address_{};
-  socklen_t address_size_{};
+  socklen_t address_size_{sizeof(client_address_)};
   int client_file_descriptor_{-1};
 };
 } // namespace Network
