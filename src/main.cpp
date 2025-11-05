75ead0a6d90138df2ee5a9a9c2de0028147f2268
Connor Garey <connorgarey1999@gmail.com>
2025-11-05 15:44:32 +0000

    Command line arguments can be put into a std::span and printed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "manager.h"
+#include <iostream>
 
-int main() {
+int main(int argc, const char* argv[]) {
   Manager server_manager{};
+
+  std::span<const char*> args(argv, argv+argc);
+  for(auto string : args)
+  {
+    std::cout << string << "\n";
+  }
 
   while(!server_manager.readyToExit())
   {
