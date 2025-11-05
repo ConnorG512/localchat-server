b35b938f1d9533500109f10104d7b3be0c04f5fd
Connor Garey <connorgarey1999@gmail.com>
2025-11-05 11:55:17 +0000

    Added arguments and the manager main loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
-#include "server/server.h"
+#include "manager.h"
 
+#include <print>
 #include <unistd.h>
 
-int main() {
-
-  Networking::Server server_instance{};
-  while(true)
+int main(int argc, char *argv[]) {
+  
+  if(argc > 1) 
   {
+    std::println("Multiple Arguments provided!");
+  }
+  Manager server_manager{};
+
+  while(!server_manager.readyToExit())
+  {
+    server_manager.mainLoop();
     sleep(1);
   }
   return 0;
