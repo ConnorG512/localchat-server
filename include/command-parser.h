2de80435e6bb2c3e4fe26269c8964a337dd71db0
Connor Garey <connorgarey1999@gmail.com>
2025-11-05 18:29:44 +0000

    Added EXIT status variables and error checking.
    .

--- include/command-parser.h
+++ include/command-parser.h
@@ -1,10 +1,11 @@
 #pragma once
+
 #include <expected>
 #include <span>
 #include <string>
 
 namespace CommandParser
 {
-[[nodiscard]] std::expected<void, std::string>OverMaxArgs(std::span<const char*> arguments);
+[[nodiscard]] std::expected<void, std::string> OverMaxArgs(std::span<const char*> arguments);
 void printAvailableArguments();
-}
+} // namespace CommandParser
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
-#include "manager.h"
 #include "command-parser.h"
+#include "manager.h"
+
+#include <cstdlib>
 #include <iostream>
 
-int main(int argc, const char* argv[]) {
-  if(const auto result = CommandParser::OverMaxArgs(std::span<const char*>(argv, argv+argc)); !result.has_value())
+int main(int argc, const char* argv[])
+{
+  if (const auto result = CommandParser::OverMaxArgs(std::span<const char*>(argv, argv + argc)); !result.has_value())
   {
-    std::cerr << result.error() << "\n"; 
-    return -1;
+    std::cerr << result.error() << "\n";
+    CommandParser::printAvailableArguments();
+    return EXIT_FAILURE;
   }
-  CommandParser::printAvailableArguments();
-  
+
   Manager server_manager{};
 
-  while(!server_manager.readyToExit())
+  while (!server_manager.readyToExit())
   {
     server_manager.mainLoop();
     sleep(1);
   }
-  return 0;
+
+  return EXIT_SUCCESS;
 }
